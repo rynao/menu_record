387316fb41934f6979b793958404c2eb93416f36
rynao <94769118+rynao@users.noreply.github.com>
2022-01-01 09:10:24 +0900

    migrationファイル修正

--- db/migrate/20211228222105_add_menu_id_to_cooking_records.rb
+++ db/migrate/20211228222105_add_menu_id_to_cooking_records.rb
@@ -1,5 +1,5 @@
 class AddMenuIdToCookingRecords < ActiveRecord::Migration[6.0]
   def change
-    add_reference :cooking_records, :menu, foreign_key: true
+    # add_reference :cooking_records, :menu, foreign_key: true
   end
 end
