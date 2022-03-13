780e53b36e81bbc7191f47a02233dc220d3ff473
rynao <94769118+rynao@users.noreply.github.com>
2022-03-13 11:29:44 +0900

    formオブジェクト実装

---
+++ app/models/menu_cooking_records.rb
@@ -0,0 +1,15 @@
+class MenuCookingRecords
+  include ActiveModel::Model
+  attr_accessor :title, :recipe_url, :memo, :user_id, :start_time, :image
+
+  validates :title, presence: true
+  validates :user_id, presence: true
+  
+  def save
+    menu = Menu.create(title: title, recipe_url: recipe_url, memo: memo, image: image, user_id: user_id)
+    if :start_time.present?
+      CookingRecord.create(start_time: start_time, user_id: user_id, menu_id: menu.id)
+    end
+  end
+
+end
