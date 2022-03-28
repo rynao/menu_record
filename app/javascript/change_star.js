859d8889bbd60348a68ce3c50b996e3399b0f93e
rynao <94769118+rynao@users.noreply.github.com>
2022-03-28 11:42:54 +0900

    星クリック時の挙動修正

--- app/javascript/change_star.js
+++ app/javascript/change_star.js
@@ -2,14 +2,29 @@ const changeStar = () => {
   const stars = document.getElementsByClassName("star");
   for (let i = 0; i < stars.length; i++) {
     stars[i].addEventListener("click", function () {
-      if (this.textContent === "☆") {
-        this.textContent = "★";
-      } else {
-        this.textContent = "☆";
+      countStars = starCount(stars);
+      num = this.dataset.star;
+      if (num < countStars) {
+        for (let i = num; i < 5; i++) {
+          stars[i].textContent = "☆";
+        }
+      } else if (num > countStars) {
+        for (let i = 0; i < num; i++) {
+          stars[i].textContent = "★";
+        }
       }
     });
   }
 
+  function starCount(stars) {
+    let starNum = 0;
+    for (let i = 0; i < stars.length; i++) {
+      if (stars[i].textContent === "★") {
+        starNum += 1;
+      }
+    }
+    return starNum;
+  }
   // const starMouseout = () => {
   //   this.value = "☆";
   // };
