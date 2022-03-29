21771afb392acf5d451fd1f3ef8f77be91b8cf3e
rynao <94769118+rynao@users.noreply.github.com>
2022-03-29 10:50:49 +0900

    menu新規登録フォームに評価カラム追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start();
 require("channels");
 require("../cooking_record");
 require("../change_star");
+require("../first_rating");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
